90bb63323514dd426c2d6462c13ec472d802cf06
Anderson <BlackCode7@outlook.com>
2022-06-26 16:49:23 -0300

    objeto celular

--- src/celular/FullStackJavaCelular.java
+++ src/celular/FullStackJavaCelular.java
@@ -1,6 +1,15 @@
 package celular;
 
 public class FullStackJavaCelular {
+	
+	String marca;
+	boolean acessoInternet;
+	boolean processador;
+	boolean capaProtecao;
+	String codigoSeguranca;
+	String dispositivoSom;
+	boolean possuiLanterna;
+	boolean modoAviao;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
