5b7c18a2def8ef0412cd963b0379e134e1cc4c9e
Anderson <BlackCode7@outlook.com>
2022-06-26 18:12:52 -0300

    criando a classe de execução que irá executar os objetos das outras classes

--- src/executavel/Execucao.java
+++ src/executavel/Execucao.java
@@ -1,6 +1,8 @@
 package executavel;
 
 public class Execucao {
+	
+	/* Classe usada pra executar e chamar os objetos de outras classes */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
