a6884a4e0e91f0d740292801e91d25f3c8047e5c
Anderson <BlackCode7@outlook.com>
2022-06-26 16:50:53 -0300

    objeto carro

---
+++ src/Carro/FullStackJavaCarro.java
@@ -0,0 +1,16 @@
+package Carro;
+
+public class FullStackJavaCarro {
+	
+	String cor;
+	int assentos;
+	int velocidade;
+	boolean importado;
+	double valor;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
