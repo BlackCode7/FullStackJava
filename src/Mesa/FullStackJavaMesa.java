ff94ed31470caba040f642ea5513b93a41730367
Anderson <BlackCode7@outlook.com>
2022-06-26 18:12:04 -0300

    completando os objetos da classe mesa

--- src/Mesa/FullStackJavaMesa.java
+++ src/Mesa/FullStackJavaMesa.java
@@ -1,6 +1,11 @@
 package Mesa;
 
 public class FullStackJavaMesa {
+	
+	String marca;
+	int medidas;
+	int numAcentos;
+	String tipomaterial;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
