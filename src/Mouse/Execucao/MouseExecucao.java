0524cbb9100b7d08ea4b99848efb03ecd4683699
Anderson <BlackCode7@outlook.com>
2022-07-15 07:12:19 -0300

    Substituindo um aluno na lista

--- src/Mouse/Execucao/MouseExecucao.java
+++ src/Mouse/Execucao/MouseExecucao.java
@@ -15,7 +15,7 @@ public class MouseExecucao {
 		/* Percorrendo o nome do aluno */
 		List<Aluno> alunos = new ArrayList<Aluno>();
 
-		for (int qtd = 1; qtd <= 4; qtd++) {
+		for (int qtd = 1; qtd <= 2; qtd++) {
 
 			String nome = JOptionPane.showInputDialog("Qual o nome do aluno" + qtd + "? ");
 			Aluno aluno1 = new Aluno();
@@ -34,6 +34,39 @@ public class MouseExecucao {
 			}
 			alunos.add(aluno1);
 		}
+
+		
+		/* Substituindo um aluno na lista */
+		for( int pos=0; pos < alunos.size(); pos++ ) {
+			
+			Aluno aluno1 = alunos.get(pos);
+			
+			if( aluno1 .getNome().equalsIgnoreCase("Anderson") ) {
+				Aluno troca = new Aluno();
+				troca.setNome("Aluno trocado");
+				
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina1("Matemática");
+				disciplina.setNota1(100);
+				
+				troca.getDisciplinas().add(disciplina);
+				
+			}
+			
+			Aluno aluno = alunos.get(pos);
+			System.out.println("aluno " + aluno.getNome());
+			System.out.println("Media do aluno " + aluno.getMedianota());
+			System.out.println("Aluno aprovado ou não " + aluno.getAlunoAprovado2());
+			System.out.println("-------------------------------------------------------");
+			
+			/* Percorrendo as disciplinas pelas posições das listas associadas ao aluno */
+			for ( int posdis=0; posdis < aluno.getDisciplinas().size(); posdis++ ) {
+				Disciplina disc = aluno.getDisciplinas().get(posdis);
+				System.out.println( "Materia = " + disc.getDisciplina1() + " Nota =  " + disc.getNota1());
+			}
+			
+		}
+
 		
 		/* Percorrendo lista pelas posições */
 		for( int pos=0; pos < alunos.size(); pos++ ) {
