c28d0b8267b976714ad7eeb52ed7d19846ea9cd8
Anderson <BlackCode7@outlook.com>
2022-07-18 07:10:40 -0300

    Uso de constantes

---
+++ src/constantesJava/Constantes.java
@@ -0,0 +1,5 @@
+package constantesJava;
+
+public class Constantes {
+
+}
---
+++ src/constantesJava/StatusAluno.java
@@ -0,0 +1,10 @@
+package constantesJava;
+
+public class StatusAluno {
+	
+	/* Constantes staticas não precisam de construtores */
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERACAO = "Recuperacao";
+	public static String REPROVADO = "Reprovado";
+	
+}
