88301de8751f2d53d49551c003f3c91a88313cf2
Anderson <BlackCode7@outlook.com>
2022-08-04 23:22:24 -0300

    iterator of list

---
+++ src/JavaNotesForProfessional/CreatingListFromArray/CreatingListFromArray.java
@@ -0,0 +1,30 @@
+package JavaNotesForProfessional.CreatingListFromArray;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CreatingListFromArray {
+
+	public static void main(String[] args) {
+
+		/*
+		 * O método Arrays.asList() pode ser usado para retornar uma lista de 
+		 * tamanho fixo contendo os elementos da matriz dada. A Lista resultante 
+		 * será do mesmo tipo de parâmetro que o tipo de base da matriz.
+		 */
+		
+		String[] stringArray = { "CasaTargeryan", "CasaStark", "CasaLanisters" };
+		List<String> stringList = Arrays.asList(stringArray);
+		
+		if( true ) {
+			
+			for(String list: stringList ) {
+				System.out.println(list);
+			}
+			
+		}
+		
+
+	}
+
+}
