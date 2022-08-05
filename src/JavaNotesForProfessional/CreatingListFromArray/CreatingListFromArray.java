4ff5e6fb75330955200c7b0533976a7a3311d2d4
Anderson <BlackCode7@outlook.com>
2022-08-04 23:43:51 -0300

    acertando o array de listas com for

--- src/JavaNotesForProfessional/CreatingListFromArray/CreatingListFromArray.java
+++ src/JavaNotesForProfessional/CreatingListFromArray/CreatingListFromArray.java
@@ -13,15 +13,15 @@ public class CreatingListFromArray {
 		 * será do mesmo tipo de parâmetro que o tipo de base da matriz.
 		 */
 		
-		String[] stringArray = { "CasaTargeryan", "CasaStark", "CasaLanisters" };
+		String[] stringArray = { "CasaTargeryan", "CasaStark", "CasaLanisters", "CasaBolton", "CasaMormont", "CasaGreyjoy", "CasaFrey", "CasaTully" };
 		List<String> stringList = Arrays.asList(stringArray);
 		
-		if( stringArray.length > 0 ) {
-			
+		if( stringArray.length > 3 && stringArray.length <= 10) {			
 			for(String list: stringList ) {
 				System.out.println(list);
-			}
-			
+			}			
+		} else {
+			System.out.println("Array não contem elementos suficientes!");
 		}
 		
 
