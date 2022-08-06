73b51ab79572dbdcff0e496184d7f0f64ba4cfaf
Anderson <BlackCode7@outlook.com>
2022-08-06 10:13:05 -0300

    Tipos de erros mais comuns em java

--- src/TratamentoErrosExcecoesJava/TratamentoErrosExcecoesJava.java
+++ src/TratamentoErrosExcecoesJava/TratamentoErrosExcecoesJava.java
@@ -5,6 +5,21 @@ public class TratamentoErrosExcecoesJava {
 	public static void main(String[] args) {
 		// Estrutura do tratamento de exceções
 		
+		/*
+		 * ERROS MAIS COMUNS EM JAVA
+		 * 
+		 * NullPointerException - quando existe variavel nula
+		 * 
+		 * NumberFormatException - quando tenta converter string em numero mas a string esta vazia
+		 * 
+		 * FileNotFoundException - quando tenta carregar um arquivo, mas o caminho do arquivo não foi encontrado
+		 * 
+		 * ClassCastException - quando tenta converter um objeto em outro, mas não sao do memso tipo ou relação
+		 * 
+		 * ClassNotFoundException - quando tenta carregar uma classe que não foi encontrada
+		 * 
+		 */
+		
 		try {
 			
 			// Bloco de comandos para executar
