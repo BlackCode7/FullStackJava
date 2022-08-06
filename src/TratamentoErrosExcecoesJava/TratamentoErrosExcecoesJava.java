06f1071f68d2ddfea73570c459ad57793cc42bd5
Anderson <BlackCode7@outlook.com>
2022-08-06 10:07:11 -0300

    Estrutura do tratamento de exceções

---
+++ src/TratamentoErrosExcecoesJava/TratamentoErrosExcecoesJava.java
@@ -0,0 +1,22 @@
+package TratamentoErrosExcecoesJava;
+
+public class TratamentoErrosExcecoesJava {
+
+	public static void main(String[] args) {
+		// Estrutura do tratamento de exceções
+		
+		try {
+			
+			// Bloco de comandos para executar
+			// Aqui dentro fica todo o código de regra de negócios da aplicação
+			
+		}catch (Exception e) {
+			
+			// Onde mostra os erros
+			e.printStackTrace();	
+			
+		}
+
+	}
+
+}
